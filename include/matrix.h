4b0427b630d031d9afbab185e63acdba44c5b144
Lebogang Masia <lebogangmasia0@gmail.com>
2025-12-29 19:24:39 +0200

    Added rule of 5 for matrix class

--- include/matrix.h
+++ include/matrix.h
@@ -17,7 +17,7 @@ namespace Lingebra {
             matrix() : nrows(0), ncols(0), data(nullptr) {}
 
             matrix(std::size_t rows, std::size_t cols) : nrows(rows), ncols(cols) {
-                data = new vector[rows];
+                data = new vector[rows]();
                 for (std::size_t i { 0 }; i < rows; i++) data[i] = vector(cols);
             }
 
@@ -26,7 +26,59 @@ namespace Lingebra {
                 std::copy(input.begin(), input.end(), data);
             }
 
+            // rule of 5 stuff, again...
+
+            // 1.
+            ~matrix() {
+                delete[] data;
+            }
+
+            // 2.
+            matrix(const matrix& other) : nrows(other.nrows), ncols(other.ncols) {
+                data = new vector[nrows];
+                std::copy(other.data, other.data + nrows, data);
+            }
+
+            // 3.
+            matrix(matrix&& other) noexcept : nrows(other.nrows), ncols(other.ncols) {
+                data = other.data; 
+                other.data = nullptr;
+                other.nrows = 0;
+                other.ncols = 0;
+            }
+
+            // 4.
+            matrix& operator=(const matrix& other) {
+                if (this == &other) return *this;
+                vector* temp = new vector[other.nrows];
+                std::copy(other.data, other.data + other.nrows, temp);
+                delete[] data;
+
+                data = temp;
+                nrows = other.nrows;
+                ncols = other.ncols;
+
+                return *this;
+            }
+
+            // 5.
+            matrix& operator=(matrix&& other) noexcept {
+                if (this == &other) return *this;
+                delete[] data;
+                data = other.data;
+                nrows = other.nrows;
+                ncols = other.ncols;
+
+                other.data = nullptr;
+                other.nrows = 0;
+                other.ncols = 0;
+
+                return *this;
+            }
+
             vector& operator[](std::size_t i) { return data[i]; };
+
+            const vector& operator[](std::size_t i) const { return data[i]; }
 
             friend std::ostream& operator<<(std::ostream&, const matrix&);
     };
